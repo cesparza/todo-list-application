8b002a921eb7fdbb2819610a8d1a3181a13e111b
Camilo Esparza <camilo.esparza@stewart.com>
2025-02-26 21:56:35 -0500

    Create a task

--- index.js
+++ index.js
@@ -14,11 +14,29 @@ function App(){
     }        
   ])
 
+  const [value, setValue]= React.useState('');
+  const handleSummit= e=>{
+    e.preventDefault();
+    if(!value) return;
+    const newTodos=[...todos,{ text:value, isCompleted:false} ];
+    setTodos(newTodos);
+    setValue('');
+  }
+
   return(
     <>
       {todos.map((todo, i) => (
         <div className="todo" key={i}>{todo.text}</div>
       ))}
+      <form onSubmit={handleSummit}>
+        <input
+            type="text"
+            className="input"
+            value={value}
+            placeholder="Add Todo ..."
+            onChange={e=> setValue(e.target.value)}
+        />
+      </form>
     </>
   );
 }
